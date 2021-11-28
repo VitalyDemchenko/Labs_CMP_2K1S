2cd5a6b829e17e55b289488724ce164b4923f52a
Vitaly Demchenko <71827388+VitalyDemchenko@users.noreply.github.com>
2021-11-28 23:30:08 +0200

    Add files via upload

---
+++ Lab_8 Числове диференціювання/kod_lab_8.py
@@ -0,0 +1,25 @@
+import math
+import numpy as np
+x= [2.4,2.6,2.8,3.0,3.2,3.4]
+y= [3.526,3.782,3.945,4.043,4.104,4.155]
+h = x[1] - x[0]
+
+def dy(y,j):
+  m1 = []
+  for i in range(len(y)):
+     m1.append(y[i] - y[i-1])
+  m1.pop(0)
+  if j == 1:
+    return m1
+  else:
+    j-=1
+    return dy(m1,j)
+#m2 = []
+#m2 = dy(y,1)
+#print(m2)
+#return m1
+#print(dy(y,j))
+yx1 = 1/h *(dy(y,1)[1] - dy(y,2) [1]/2 + dy(y,3) [1]/3 - dy(y,4) [1]/4)
+yx2 = 1/h**2*(dy(y,2) [1] - dy(y,3) [1] + 11/12*dy(y,4) [1])
+print(' df(x) = ', yx1)
+print(' df2(x) = ', yx2)
